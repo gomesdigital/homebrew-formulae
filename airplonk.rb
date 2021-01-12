b4c612e0609dc09adf880897c80bde83ed52f7a9
daaanstraight <daaanstraight@gmail.com>
2021-01-12 20:44:39 +1300

    Ammended release source to the latest release.

--- airplonk.rb
+++ airplonk.rb
@@ -1,14 +1,20 @@
-class Airplonk < Formula
-  desc "Plonk stuff around your local net."
-  homepage "https://github.com/daaanstraight"
-  url "https://github.com/daaanstraight/AirPlonk/archive/v1.0.1.tar.gz"
-  version "1.0.0"
-  sha256 "c5355b5f0e6e7823bee0b0981f5504f95e14c90a43dbc7a13d4ba813aa4ac7d5"
-  license "GPL-3.0"
+# typed: strong
+# frozen_string_literal: true
 
+# A Homebrew formula for AirPlonk, https://github.com/daaanstraight/AirPlonk
+class Airplonk < Formula
+  desc "Plonk stuff around your local net"
+  homepage "https://github.com/daaanstraight"
+  url "https://github.com/daaanstraight/AirPlonk/archive/v1.0.2.tar.gz"
+  sha256 "7365ec626b2bc4660e5b5eba5a866548f62d21d42bb6e39633d9d7fb1eaee266"
+  license "GPL-3.0-only"
+  depends_on "awk"
+  depends_on "expect"
+  depends_on "grep"
+  depends_on "nmap"
+  depends_on "rsync"
   def install
     bin.install "airplonk"
     man1.install "doc/airplonk.1"
   end
-
 end

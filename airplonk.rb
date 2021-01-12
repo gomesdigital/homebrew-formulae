e8b00723a9cc22282cc746272e65d83636775152
daaanstraight <daaanstraight@gmail.com>
2021-01-12 18:58:17 +1300

    Ammended incorrect SHA sum.

--- airplonk.rb
+++ airplonk.rb
@@ -3,7 +3,7 @@ class Airplonk < Formula
   homepage "https://github.com/daaanstraight"
   url "https://github.com/daaanstraight/AirPlonk/archive/v1.0.1.tar.gz"
   version "1.0.0"
-  sha256 "7ff2e73df491e706e5407ecc881c1d2674d4a146e826e4635c8257d86d730095"
+  sha256 "c5355b5f0e6e7823bee0b0981f5504f95e14c90a43dbc7a13d4ba813aa4ac7d5"
   license "GPL-3.0"
 
   def install

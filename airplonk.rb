7cfba0088a0febf952871d2b1ce9cedf19d8bd8a
daaanstraight <daaanstraight@gmail.com>
2021-01-20 18:05:30 +1300

    Ammended incorrect shasum.

--- airplonk.rb
+++ airplonk.rb
@@ -6,7 +6,7 @@ class Airplonk < Formula
   desc "Plonk stuff around your local net"
   homepage "https://github.com/daaanstraight"
   url "https://github.com/daaanstraight/AirPlonk/archive/v1.0.1.tar.gz"
-  sha256 "490563f02f635b7a6a3f8a85e453a487c0d2a79e3c776fb932c6d3d95666f00f"
+  sha256 "1e663473e5aca446d7fbc5c2a0a7435a46b5b73be3978a702f777b0acad167f1"
   license "GPL-3.0-only"
   depends_on "awk"
   depends_on "expect"

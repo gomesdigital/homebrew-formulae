450896e5a6f07032caef919a592a6407ba499d48
daaanstraight <daaanstraight@gmail.com>
2021-01-20 15:54:31 +1300

    Updated release source to the latest release.

--- airplonk.rb
+++ airplonk.rb
@@ -5,8 +5,8 @@
 class Airplonk < Formula
   desc "Plonk stuff around your local net"
   homepage "https://github.com/daaanstraight"
-  url "https://github.com/daaanstraight/AirPlonk/archive/v1.0.3.tar.gz"
-  sha256 "eeef13e0965bf1afe53ffac3c8397fe83c7fe0990ce8ffc0ba26864d5f2c364b"
+  url "https://github.com/daaanstraight/AirPlonk/archive/v1.0.1.tar.gz"
+  sha256 "490563f02f635b7a6a3f8a85e453a487c0d2a79e3c776fb932c6d3d95666f00f"
   license "GPL-3.0-only"
   depends_on "awk"
   depends_on "expect"

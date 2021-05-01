f30a84f327b5b53528367f11691d7f552f167461
Daniel Gomes-Sebastiao <daaanstraight@gmail.com>
2021-05-02 00:35:39 +1200

    Updated source url to latest release.

--- airplonk.rb
+++ airplonk.rb
@@ -5,8 +5,8 @@
 class Airplonk < Formula
   desc "Plonk stuff around your local net"
   homepage "https://github.com/daaanstraight"
-  url "https://github.com/gomesdigital/AirPlonk/archive/v1.0.0.tar.gz"
-  sha256 "80e39015e5fe3c5fc8d066bcb2ead6eee40a2e6b0707538bce286dccc84e3929"
+  url "https://github.com/gomesdigital/AirPlonk/archive/v1.0.1.tar.gz"
+  sha256 "29ef77a26876b026c084f7d5d09fd13d087af61638cbbfed45e9f7ab392ea748"
   license "GPL-3.0-only"
   depends_on "awk"
   depends_on "expect"

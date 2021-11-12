f3e1702a5de21a26d24bfb0c0637cf3744419b99
Daniel Gomes-Sebastiao <daniel@gomes.digital>
2021-11-12 23:11:36 +1300

    Updated tag hash

--- airplonk.rb
+++ airplonk.rb
@@ -6,7 +6,7 @@ class Airplonk < Formula
   desc "Plonk stuff around your local net"
   homepage "https://github.com/gomesdigital"
   url "https://github.com/gomesdigital/AirPlonk/archive/v1.0.2.tar.gz"
-  sha256 "1c08b25b805f88b28b496b88b42ab9905b0c5b0f5704af3e51f6b74515dc9eac"
+  sha256 "8250594554750ee8223b97147b83c6afe6cdc51323131a5f7c890611959e70cc"
   license "GPL-3.0-only"
   depends_on "awk"
   depends_on "expect"

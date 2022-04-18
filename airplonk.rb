47d895d89c32b0684cbd60392a7ef11944e80c34
gomesdigital <daniel@gomes.digital>
2022-04-18 18:18:37 +1200

    update formula to v1.1.0

--- airplonk.rb
+++ airplonk.rb
@@ -3,18 +3,20 @@
 
 # A Homebrew formula for AirPlonk, https://github.com/gomesdigital/AirPlonk
 class Airplonk < Formula
-  desc "Plonk stuff around your local net"
+  desc "Plonk stuff around your local net."
   homepage "https://github.com/gomesdigital"
-  url "https://github.com/gomesdigital/AirPlonk/archive/v1.0.2.tar.gz"
-  sha256 "1c08b25b805f88b28b496b88b42ab9905b0c5b0f5704af3e51f6b74515dc9eac"
-  license "GPL-3.0-only"
+  url "https://github.com/gomesdigital/AirPlonk/archive/v1.1.0.tar.gz"
+  sha256 "20b5a01a13c5956f2171d3b7593aa6543c262f715a47682c76480ab58719a8ed"
+  license "GPL-2.0-only"
   depends_on "awk"
   depends_on "expect"
   depends_on "grep"
   depends_on "nmap"
   depends_on "rsync"
   def install
-    bin.install "airplonk"
+    bin.install "src/airplonk"
+    bin.install "src/airplonk-animations"
+    bin.install "src/airplonk-manual"
     man1.install "doc/airplonk.1"
   end
 end
